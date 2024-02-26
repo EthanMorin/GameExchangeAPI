94894cc33edaaa37303dca1e784f0f94c2d810b3
EthanMorin <EthanMorin2004@gmail.com>
2024-02-26 11:49:31 -0700

    update emailSenderService

--- games_email/main.go
+++ games_email/main.go
@@ -1,57 +1,61 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"os/signal"
-	"time"
+import "games-email/services"
 
-	"github.com/IBM/sarama"
-)
+// "fmt"
+// "log"
+// "os"
+// "os/signal"
+// "time"
+
+// "github.com/IBM/sarama"
+
+// func main() {
+// time.Sleep(time.Second * 5)
+// brokers := []string{"kafka:9092"}
+// topics := []string{"users", "trades"}
+
+// config := sarama.NewConfig()
+// config.Consumer.Return.Errors = true
+
+// consumer, err := sarama.NewConsumer(brokers, config)
+// if err != nil {
+// 	log.Panicf("Error creating consumer: %v", err)
+// }
+// defer consumer.Close()
+
+// messages := make(chan *sarama.ConsumerMessage)
+
+// for _, topic := range topics {
+// 	partitions, err := consumer.Partitions(topic)
+// 	if err != nil {
+// 		log.Panicf("Error getting partitions for topic %s: %v", topic, err)
+// 	}
+
+// 	for _, partition := range partitions {
+// 		pc, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+// 		if err != nil {
+// 			log.Panicf("Error consuming partition %d of topic %s: %v", partition, topic, err)
+// 		}
+// 		go func(pc sarama.PartitionConsumer) {
+// 			for msg := range pc.Messages() {
+// 				messages <- msg
+// 			}
+// 		}(pc)
+// 	}
+// }
+
+// go func() {
+// 	for msg := range messages {
+// 		fmt.Printf("Received message: topic=%s, partition=%d, offset=%d, value=%s\n", msg.Topic, msg.Partition, msg.Offset, string(msg.Value))
+// 	}
+// }()
+
+// sigterm := make(chan os.Signal, 1)
+// signal.Notify(sigterm, os.Interrupt)
+// <-sigterm
+// }
 
 func main() {
-	time.Sleep(time.Second * 5)
-	brokers := []string{"kafka:9092"}
-	topics := []string{"users", "trades"}
-
-	config := sarama.NewConfig()
-	config.Consumer.Return.Errors = true
-
-	consumer, err := sarama.NewConsumer(brokers, config)
-	if err != nil {
-		log.Panicf("Error creating consumer: %v", err)
-	}
-	defer consumer.Close()
-
-	messages := make(chan *sarama.ConsumerMessage)
-
-	for _, topic := range topics {
-		partitions, err := consumer.Partitions(topic)
-		if err != nil {
-			log.Panicf("Error getting partitions for topic %s: %v", topic, err)
-		}
-
-		for _, partition := range partitions {
-			pc, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
-			if err != nil {
-				log.Panicf("Error consuming partition %d of topic %s: %v", partition, topic, err)
-			}
-			go func(pc sarama.PartitionConsumer) {
-				for msg := range pc.Messages() {
-					messages <- msg
-				}
-			}(pc)
-		}
-	}
-
-	go func() {
-		for msg := range messages {
-			fmt.Printf("Received message: topic=%s, partition=%d, offset=%d, value=%s\n", msg.Topic, msg.Partition, msg.Offset, string(msg.Value))
-		}
-	}()
-
-	sigterm := make(chan os.Signal, 1)
-	signal.Notify(sigterm, os.Interrupt)
-	<-sigterm
+	services.SendEmail("Ethanmorin2004@gmail.com", "Test", "Test")
 }
--- games_email/services/email.go
+++ games_email/services/email.go
@@ -1,1 +1,32 @@
 package services
+
+// Ethereal Email
+// Email: kurt.heaney@ethereal.email
+// Pass: 2g8QsnyNdt2zhChpQ8
+
+import (
+	"log"
+
+	"github.com/wneessen/go-mail"
+)
+
+func SendEmail(email string, subject string, body string) {
+	message := mail.NewMsg()
+	if err := message.From("kurt.heaney@ethereal.email"); err != nil {
+		log.Fatalf("Failed to set sender: {%s}", err)
+	}
+	if err := message.To(email); err != nil {
+		log.Fatalf("Failed to set recipient: {%s}", err)
+	}
+	message.Subject(subject)
+	message.SetBodyString(mail.TypeTextPlain, body)
+
+	c, err := mail.NewClient("smtp.ethereal.email", mail.WithPort(587), mail.WithSMTPAuth(mail.SMTPAuthPlain),
+		mail.WithUsername("kurt.heaney@ethereal.email"), mail.WithPassword("2g8QsnyNdt2zhChpQ8"))
+	if err != nil {
+		log.Fatalf("failed to create mail client: {%s}", err)
+	}
+	if err := c.DialAndSend(message); err != nil {
+		log.Fatalf("failed to send mail: {%s}", err)
+	}
+}
